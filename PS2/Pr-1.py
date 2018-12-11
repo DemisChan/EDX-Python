5e59a0c754001e71110fe5e15c691ca9501fde1a
DEMIS LAVRENTIDIS <30480177+DemisChan@users.noreply.github.com>
2018-12-11 01:00:14 +0000

    Create Pr-1.py

---
+++ PS2/Pr-1.py
@@ -0,0 +1,34 @@
+#Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
+#The following variables contain values as described below:
+#balance - the outstanding balance on the credit card
+#annualInterestRate - annual interest rate as a decimal
+#monthlyPaymentRate - minimum monthly payment rate as a decimal
+#For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining balance. Be sure to print out no more than two decimal digits of accuracy - so print
+#Remaining balance: 813.41
+#instead of
+#Remaining balance: 813.4141998135 
+#So your program only prints out one thing: the remaining balance at the end of the year in the format:
+#Remaining balance: 4784.0
+#A summary of the required math is found below:
+#Monthly interest rate= (Annual interest rate) / 12.0
+#Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
+#Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
+#Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
+
+b0 = balance
+p = monthlyPaymentRate
+apr = annualInterestRate
+mpr = apr/12
+def ccb(b0, apr, p):
+    '''
+    credit card balance at end of year
+    '''
+    ub = b0 - p*b0
+    mb = ub+mpr*ub
+    return mb
+ub = ccb(b0, apr, p) 
+i = 0
+for i in range(11):
+    ub = ccb(ub, apr, p)
+    i+=1
+print('Remaining balance:', round(ub, 2))
