09c600737e29bd674ac9882af56bc8433e6254bb
DEMIS LAVRENTIDIS <30480177+DemisChan@users.noreply.github.com>
2018-12-13 00:16:39 +0000

    Create Pr-5.py

---
+++ Mid-Term/Pr-5.py
@@ -0,0 +1,34 @@
+def applyF_filterG(L, f, g):
+    """
+    Assumes L is a list of integers
+    Assume functions f and g are defined for you. 
+    f takes in an integer, applies a function, returns another integer 
+    g takes in an integer, applies a Boolean function, 
+        returns either True or False
+    Mutates L such that, for each element i originally in L, L contains  
+        i if g(f(i)) returns True, and no other elements
+    Returns the largest element in the mutated L or -1 if the list is empty
+    """
+    x = []
+    for i in L:
+        if g(f(i)) == True:
+            x.append(i)
+    L[:] = x
+    if len(L) == 0:
+            return -1
+    else:
+        return max(L)
+
+def f(i):
+    return i + 2
+def g(i):
+    return i > 5
+
+L = [0, -10, 5, 6, -4]
+print(applyF_filterG(L, f, g))
+print(L)
+
+
+#result should be
+#6
+#[5, 6]
