5cc40a713b2ff93458dbadd4f5b61a0e9ca5e00f
DEMIS LAVRENTIDIS <30480177+DemisChan@users.noreply.github.com>
2018-12-13 00:01:14 +0000

    Create Pr-3.py

---
+++ PS3/Pr-3.py
@@ -0,0 +1,22 @@
+#Next, implement the function getAvailableLetters that takes in one parameter - a list of letters, lettersGuessed. This function returns a string that is comprised of lowercase English letters - all lowercase English letters that are not in lettersGuessed.
+#Example Usage:
+#>>> lettersGuessed = ['e', 'i', 'k', 'p', 'r', 's']
+#>>> print(getAvailableLetters(lettersGuessed))
+#abcdfghjlmnoqtuvwxyz
+#Note that this function should return the letters in alphabetical order, as in the example above.
+#For this function, you may assume that all the letters in lettersGuessed are lowercase.
+
+def getAvailableLetters(lettersGuessed):
+    '''
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters that represents what letters have not
+      yet been guessed.
+    '''
+    import string
+    a = set(string.ascii_lowercase)
+    b = a.difference(lettersGuessed)
+    c = sorted(list(b))
+    l = ''
+    for i in c:
+        l += i
+    return l
